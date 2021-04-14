9d53dc914539464a14d2b811908a1dc0c7eaddb9
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-15 01:13:02 +0900

    Update: Add new class member category

--- online_supermarket/lib/models/item.dart
+++ online_supermarket/lib/models/item.dart
@@ -1,27 +1,37 @@
+enum MyCategory {
+  vegetables,
+  meat,
+  fruits,
+}
+
 class Item {
   Item({
     this.name,
     this.imagePath,
     this.count,
     this.price,
+    this.category,
   });
 
   String name;
   String imagePath;
   int count = 0;
   int price;
+  MyCategory category;
 
   Item copyWith({
     String name,
     String imagePath,
     int count,
     int price,
+    MyCategory category,
   }) {
     return Item(
       name: name ?? this.name,
       imagePath: imagePath ?? this.imagePath,
       count: count ?? this.count,
       price: price ?? this.price,
+      category: category ?? this.category,
     );
   }
 }
