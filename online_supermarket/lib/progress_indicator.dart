aff5a990ba6b8f7796b9ac1738655b811c35e055
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-16 17:19:30 +0900

    Update: Add progress indicator

---
+++ online_supermarket/lib/progress_indicator.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// Reference: https://www.greycastle.se/loading-overlay-in-flutter/
+
+class LoadingOverlay {
+  LoadingOverlay._create(
+    this._context,
+  );
+  factory LoadingOverlay.of(BuildContext context) {
+    return LoadingOverlay._create(context);
+  }
+  BuildContext _context;
+
+  void hide() {
+    Navigator.of(_context).pop();
+  }
+
+  void show() {
+    showDialog<_FullScreenLoader>(
+      context: _context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return _FullScreenLoader();
+      },
+    );
+  }
+
+  Future<void> during({int milliseconds}) {
+    show();
+    Timer(Duration(milliseconds: milliseconds), hide);
+  }
+}
+
+class _FullScreenLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
