24fb6e03ea022fe3f7b8632018f039d141cf519d
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-18 12:32:46 +0900

    Update: Create new files
    - あとでtoday や tomorrow のようなカテゴリー一覧のページ作成用に準備

---
+++ to_do_list_with_riverpod/lib/view/category_page/category_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../riverpod/providers.dart';
+
+class CategoryPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Container();
+  }
+}
