f7e8958d3e8271ef4f434b9f7a9d80dd6d21a071
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-18 12:30:34 +0900

    Update: Create new riverpodNotifier and separate ToDoList

--- to_do_list_with_riverpod/lib/riverpod/providers.dart
+++ to_do_list_with_riverpod/lib/riverpod/providers.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:to_do_list_with_riverpod/view/home_page_notifier.dart';
+import 'package:to_do_list_with_riverpod/riverpod/to_do_list_notifier.dart';
+import 'package:to_do_list_with_riverpod/view/home_page/home_page_model.dart';
 
-final homePageProvider = ChangeNotifierProvider((ref) => HomePageNotifier());
+final homePageProvider = ChangeNotifierProvider((ref) => HomePageModel());
+final toDoListProvider = ChangeNotifierProvider((ref) => ToDoListNotifier());
