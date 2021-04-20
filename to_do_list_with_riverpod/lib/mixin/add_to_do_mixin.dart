0373a03fba8e22b4b0bc285b420fe12d50bc7823
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-20 16:31:58 +0900

    Update: Create new mixin AddToDoMixin and move common functions

---
+++ to_do_list_with_riverpod/lib/mixin/add_to_do_mixin.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:to_do_list_with_riverpod/model/to_do_item.dart';
+
+mixin AddToDoMixin on ChangeNotifier {
+  String _content = '';
+  String get content => _content;
+
+  TextEditingController _textEditingController;
+  TextEditingController get textEditingController => _textEditingController;
+
+  bool _isActive;
+  bool get isActive => _isActive;
+
+  Deadline _selectedDeadlineType;
+  Deadline get selectedDeadlineType => _selectedDeadlineType;
+
+  void initialize() {
+    _selectedDeadlineType = Deadline.unselected;
+    _textEditingController = TextEditingController();
+    _isActive = false;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  void onChanged(String content) {
+    _content = content;
+    if (_content.isNotEmpty) {
+      _isActive = true;
+    } else {
+      _isActive = false;
+    }
+    notifyListeners();
+  }
+
+  void onTapSubmitButton(BuildContext context) {
+    _textEditingController.clear();
+    _content = '';
+    _selectedDeadlineType = Deadline.unselected;
+    _isActive = false;
+    notifyListeners();
+  }
+
+  void onTapSelectDeadlineCard(Deadline deadline) {
+    _selectedDeadlineType = deadline;
+    notifyListeners();
+  }
+
+  void resetSelectedDeadlineCard() {
+    _selectedDeadlineType = Deadline.unselected;
+    notifyListeners();
+  }
+
+  void resetModalBottomSheet() {
+    _textEditingController.clear();
+    _content = '';
+    _selectedDeadlineType = Deadline.unselected;
+    _isActive = false;
+    notifyListeners();
+  }
+}
--- to_do_list_with_riverpod/lib/view/category_page/category_page_model.dart
+++ to_do_list_with_riverpod/lib/view/category_page/category_page_model.dart
@@ -1,72 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_with_riverpod/mixin/add_to_do_mixin.dart';
 import 'package:to_do_list_with_riverpod/mixin/get_deadline_details_mixin.dart';
 import 'package:to_do_list_with_riverpod/model/to_do_item.dart';
 import 'package:to_do_list_with_riverpod/view/home_page/home_page.dart';
 
-class CategoryPageModel extends ChangeNotifier with GetDeadlineDetailsMixin {
+class CategoryPageModel extends ChangeNotifier
+    with GetDeadlineDetailsMixin, AddToDoMixin {
   CategoryPageModel() {
-    _initialize();
-  }
-
-  String _content = '';
-  String get content => _content;
-
-  TextEditingController _textEditingController;
-  TextEditingController get textEditingController => _textEditingController;
-
-  bool _isActive;
-  bool get isActive => _isActive;
-
-  Deadline _selectedDeadlineType;
-  Deadline get selectedDeadlineType => _selectedDeadlineType;
-
-  void _initialize() {
-    _selectedDeadlineType = Deadline.unselected;
-    _textEditingController = TextEditingController();
-    _isActive = false;
-    notifyListeners();
-  }
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
-
-  void onTapSelectDeadlineCard(Deadline deadline) {
-    _selectedDeadlineType = deadline;
-    notifyListeners();
-  }
-
-  void resetSelectedDeadlineCard() {
-    _selectedDeadlineType = Deadline.unselected;
-    notifyListeners();
-  }
-
-  void onChanged(String content) {
-    _content = content;
-    if (_content.isNotEmpty) {
-      _isActive = true;
-    } else {
-      _isActive = false;
-    }
-    notifyListeners();
-  }
-
-  void onTapSubmitButton(BuildContext context) {
-    _textEditingController.clear();
-    _content = '';
-    _selectedDeadlineType = Deadline.unselected;
-    _isActive = false;
-    notifyListeners();
-  }
-
-  void resetModalBottomSheet() {
-    _textEditingController.clear();
-    _content = '';
-    _selectedDeadlineType = Deadline.unselected;
-    _isActive = false;
-    notifyListeners();
+    initialize();
   }
 
   void onTapDeadlineListItem(BuildContext context, Deadline deadlineType) {
