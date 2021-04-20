23ba6cf25ca5215d56d1033676b066a80633ebea
Kotaro Kamashima <kotaro0726@gmail.com>
2021-04-20 15:53:19 +0900

    Update: Add FloatingActionButton on CategoryPage

--- to_do_list_with_riverpod/lib/view/category_page/category_page.dart
+++ to_do_list_with_riverpod/lib/view/category_page/category_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:to_do_list_with_riverpod/model/to_do_item.dart';
 import 'package:to_do_list_with_riverpod/utilitiy/utilities.dart';
+import 'package:to_do_list_with_riverpod/view/home_page/home_page.dart';
 
 import '../../riverpod/providers.dart';
 
@@ -10,6 +11,7 @@ class CategoryPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final notifier = watch(deadlineListProvider);
+    final model = watch(categoryPageProvider);
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -41,7 +43,118 @@ class CategoryPage extends ConsumerWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () async {
+          await _buildOnTapAddNewToDoButton(
+            context,
+            model.resetModalBottomSheet,
+          );
+        },
+      ),
     );
+  }
+
+  Future<void> _buildOnTapAddNewToDoButton(
+    BuildContext context,
+    VoidCallback resetModalBottomSheet,
+  ) {
+    return showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Consumer(
+          builder: (BuildContext context, watch, child) {
+            final model = watch(categoryPageProvider);
+            final listNotifier = watch(deadlineListProvider);
+            return GestureDetector(
+              onTap: () => model.resetSelectedDeadlineCard,
+              child: SizedBox(
+                height: 150,
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                    color: Colors.black26,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextField(
+                          controller: model.textEditingController,
+                          onChanged: model.onChanged,
+                          autofocus: true,
+                          cursorColor: Colors.red,
+                          cursorHeight: 20,
+                          decoration: const InputDecoration(
+                            hintText: 'Add a task',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                            ),
+                            border: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.transparent),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          DeadlineCard(deadlineType: Deadline.today),
+                          DeadlineCard(deadlineType: Deadline.tomorrow),
+                          DeadlineCard(deadlineType: Deadline.thisWeek),
+                          DeadlineCard(deadlineType: Deadline.thisMonth),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 15, bottom: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor:
+                                  model.isActive ? Colors.red : Colors.grey,
+                              radius: 15,
+                              child: IconButton(
+                                padding: const EdgeInsets.all(0),
+                                splashColor: Colors.transparent,
+                                color: Colors.white,
+                                icon: const Icon(Icons.arrow_upward_sharp),
+                                onPressed: () {
+                                  if (model.isActive) {
+                                    listNotifier.addNewToDoItem(
+                                        content: model.content,
+                                        selectedDeadlineType:
+                                            model.selectedDeadlineType);
+                                    model.onTapSubmitButton(context);
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    ).whenComplete(() {
+      resetModalBottomSheet();
+    });
   }
 }
 
@@ -88,6 +201,54 @@ class DeadlineListItem extends ConsumerWidget {
                 ),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DeadlineCard extends ConsumerWidget {
+  const DeadlineCard({
+    this.deadlineType,
+    Key key,
+  }) : super(key: key);
+
+  final Deadline deadlineType;
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final model = watch(categoryPageProvider);
+    final color = model.getDeadlineColor(deadlineType);
+    return GestureDetector(
+      onTap: () => model.onTapSelectDeadlineCard(deadlineType),
+      child: SizedBox(
+        height: 40,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: deadlineType == model.selectedDeadlineType
+                  ? color
+                  : Colors.grey.shade800,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${model.getDeadlineText(deadlineType)}',
+                    style: TextStyle(
+                      color: color,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- to_do_list_with_riverpod/lib/view/category_page/category_page_model.dart
+++ to_do_list_with_riverpod/lib/view/category_page/category_page_model.dart
@@ -1,8 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_with_riverpod/constants/constant.dart';
 import 'package:to_do_list_with_riverpod/model/to_do_item.dart';
 import 'package:to_do_list_with_riverpod/view/home_page/home_page.dart';
 
 class CategoryPageModel extends ChangeNotifier {
+  CategoryPageModel() {
+    _initialize();
+  }
+
+  String _content = '';
+  String get content => _content;
+
+  TextEditingController _textEditingController;
+  TextEditingController get textEditingController => _textEditingController;
+
+  bool _isActive;
+  bool get isActive => _isActive;
+
+  Deadline _selectedDeadlineType;
+  Deadline get selectedDeadlineType => _selectedDeadlineType;
+
+  void _initialize() {
+    _selectedDeadlineType = Deadline.unselected;
+    _textEditingController = TextEditingController();
+    _isActive = false;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  void onTapSelectDeadlineCard(Deadline deadline) {
+    _selectedDeadlineType = deadline;
+    notifyListeners();
+  }
+
+  String getDeadlineText(Deadline deadline) {
+    if (deadline == Deadline.today) {
+      return 'Today';
+    } else if (deadline == Deadline.tomorrow) {
+      return 'Tomorrow';
+    } else if (deadline == Deadline.thisWeek) {
+      return 'This Week';
+    } else if (deadline == Deadline.thisMonth) {
+      return 'This Month';
+    } else if (deadline == Deadline.unselected) {
+      return 'unSelected';
+    } else {
+      return '';
+    }
+  }
+
+  Color getDeadlineColor(Deadline deadline) {
+    if (deadline == Deadline.today) {
+      return kTodayColor;
+    } else if (deadline == Deadline.tomorrow) {
+      return kTomorrowColor;
+    } else if (deadline == Deadline.thisWeek) {
+      return kThisWeekColor;
+    } else if (deadline == Deadline.thisMonth) {
+      return kThisMonthColor;
+    } else if (deadline == Deadline.unselected) {
+      return Colors.grey.shade800;
+    } else {
+      return Colors.white;
+    }
+  }
+
+  void resetSelectedDeadlineCard() {
+    _selectedDeadlineType = Deadline.unselected;
+    notifyListeners();
+  }
+
+  void onChanged(String content) {
+    _content = content;
+    if (_content.isNotEmpty) {
+      _isActive = true;
+    } else {
+      _isActive = false;
+    }
+    notifyListeners();
+  }
+
+  void onTapSubmitButton(BuildContext context) {
+    _textEditingController.clear();
+    _content = '';
+    _selectedDeadlineType = Deadline.unselected;
+    _isActive = false;
+    notifyListeners();
+  }
+
+  void resetModalBottomSheet() {
+    _textEditingController.clear();
+    _content = '';
+    _selectedDeadlineType = Deadline.unselected;
+    _isActive = false;
+    notifyListeners();
+  }
+
   void onTapDeadlineListItem(BuildContext context, Deadline deadlineType) {
     Navigator.pushNamed(
       context,
